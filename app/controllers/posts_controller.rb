9738da793b4c551e29fffcf8e1adc43e363dceda
Jeroen B <jeroenb04@live.com>
2022-03-15 14:05:48 +0100

    Posts setup for page creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @post = Post.all
+    @posts = Post.all
   end
 
   def show
