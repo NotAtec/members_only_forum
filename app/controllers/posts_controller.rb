91be0b838b9355d725a259d30202bcb1d8f46518
Jeroen B <jeroenb04@live.com>
2022-03-17 14:15:46 +0100

    Fix link to New Post Page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,14 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
-    @posts = Post.all.order("created_at DESC")
+    @post = Post.new
   end
 
   def create
