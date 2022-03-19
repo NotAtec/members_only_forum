f3fc0734c59ee33a056494defac1477e289f49cf
Jeroen B <jeroenb04@live.com>
2022-03-19 14:30:59 +0100

    Allow Username Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    sign_up_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    update_attrs = [:username, :email, :password, :password_confirmation, :current_password]
+    devise_parameter_sanitizer.permit :sign_up, keys: sign_up_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: update_attrs
   end
 end
